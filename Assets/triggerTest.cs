92c27953ed1a16aacb6b1a7702a796c49547f99b
Karthik Patanjali <knp275@nyu.edu>
2016-10-27 15:01:27 -0400

    Working trigger - Important!!!

---
+++ Assets/triggerTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerTest : MonoBehaviour {
+	void Start() {
+		//		otherScript.DoSomething();
+	}
+
+	void OnTriggerEnter ( Collider other ){
+		Component halo = GetComponent("Halo");
+		halo.GetType().GetProperty("enabled").SetValue(halo, true, null);
+		gameObject.GetComponent<ParticleSystem>().Play();
+		Debug.Log ("JetPack picked!");
+		//		GameObject vrMove = GameObject.Find ("CardboardMain");
+		//		VRMoveController vrMoveLoc = vrMove.GetComponent<VRMoveController>();
+		//		vrMoveLoc.hangTime();
+		//		Destroy(this.gameObject);
+	}
+}
