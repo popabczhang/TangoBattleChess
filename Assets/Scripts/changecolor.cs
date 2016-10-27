5e9de25abc793741fb1a0b2b388bf57711092260
Muso <muso.fan@gmail.com>
2016-10-27 16:26:22 -0400

    Muso move
    Move

---
+++ Assets/Scripts/changecolor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class changecolor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("cube") || other.gameObject.CompareTag ("cube1")) {
+			other.gameObject.GetComponent<Renderer> ().material.color = new Color (255, 0, 0, 250);
+		}
+	} 
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.CompareTag ("cube")) {
+			other.gameObject.GetComponent<Renderer> ().material.color = new Color (255, 255, 255, 250);
+		} 
+		if (other.gameObject.CompareTag ("cube1")){
+			other.gameObject.GetComponent<Renderer> ().material.color = new Color (0, 0, 0, 250);
+		}
+	}
+}
