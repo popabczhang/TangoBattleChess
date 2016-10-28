1865fe00ce85bfc4cf57430211e4dabc9668932b
Ryan" Yan Zhang <popabczhang@gmail.com>
2016-10-28 11:22:28 -0400

    highlight available moves, not finished

--- Assets/Scripts/changecolor.cs
+++ Assets/Scripts/changecolor.cs
@@ -9,6 +9,10 @@ public class changecolor : MonoBehaviour {
     public GameObject[] PiecesPlayer1;
     public GameObject[] PiecesPlayer2;
 
+    private int x, y, i, j, n;
+    private Vector3 pos;
+
+
     // Use this for initialization
     void Start () {
 	}
@@ -21,8 +25,28 @@ public class changecolor : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag ("cube") || other.gameObject.CompareTag ("cube1")) {
 			other.gameObject.GetComponent<Renderer> ().material.color = new Color (255, 0, 0, 250);
-		}
-	} 
+
+            pos = other.gameObject.transform.localPosition;
+            x = Mathf.RoundToInt(pos.x + 1);
+            y = Mathf.RoundToInt(pos.z);
+            Debug.Log("x = " + x + "; y = " + y);
+
+            n = y * 8 + x;
+            for (i = y + 1; i <= 7; i++)
+            {
+                if (x - i >= 0)
+                {
+                    Cells[8 * i - (i-y)].GetComponent<Renderer>().material.color = new Color(0, 255, 0, 255);
+                }
+                if (x + i <= 7)
+                {
+                    Cells[8 * i + (i-y)].GetComponent<Renderer>().material.color = new Color(0, 255, 0, 255);
+                }
+            }
+        }
+        
+
+    } 
 
 	void OnTriggerExit(Collider other) {
 		if (other.gameObject.CompareTag ("cube")) {
@@ -31,5 +55,31 @@ public class changecolor : MonoBehaviour {
 		if (other.gameObject.CompareTag ("cube1")){
 			other.gameObject.GetComponent<Renderer> ().material.color = new Color (0, 0, 0, 250);
 		}
-	}
+
+
+        pos = other.gameObject.transform.localPosition;
+        x = Mathf.RoundToInt(pos.x + 1);
+        y = Mathf.RoundToInt(pos.z);
+        Debug.Log("x = " + x + "; y = " + y);
+
+        n = y * 8 + x;
+        for (i = y + 1; i <= 7; i++)
+        {
+            if (x - i >= 0)
+            {
+                if (Cells[8 * i - (i - y)].CompareTag("cube"))
+                {
+                    Cells[8 * i - (i - y)].GetComponent<Renderer>().material.color = new Color(255, 255, 255, 250);
+                }
+            }
+            if (x + i <= 7)
+            {
+                if (Cells[8 * i + (i - y)].CompareTag("cube1"))
+                {
+                    Cells[8 * i + (i - y)].GetComponent<Renderer>().material.color = new Color(0, 0, 0, 250);
+                }
+
+            }
+        }
+    }
 }
