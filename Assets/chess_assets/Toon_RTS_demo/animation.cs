dd2fd6c9f4c2ce5540aec1c65bd0fd16803cbdf2
Muso <muso.fan@gmail.com>
2016-10-28 14:37:20 -0400

    Animator in

---
+++ Assets/chess_assets/Toon_RTS_demo/animation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class animation : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("1")){
+			anim.Play ("WK_heavy_infantry_08_attack_B",-1,0f);
+		}
+	}
+
+//	void onTriggerEnter (Collider other){
+//		if (other.gameObject.CompareTag ("Player")) {
+//			anim.Play ("WK_heavy_infantry_08_attack_B", -1, 0f);
+//		}
+//	}
+}
