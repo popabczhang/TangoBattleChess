0fab3243d754c9ffc3e50377c7fbd19bda233472
Karthik Patanjali <knp275@nyu.edu>
2016-10-30 22:51:51 -0400

    Fixed multiple selection issue
    Fixed the problem of carrying multiple pieces at once. Attached a
    script to the player to check if he is carrying a piece.

---
+++ Assets/networkedTouchBool.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class networkedTouchBool : MonoBehaviour {
+
+public bool networkTouch = false;
+public bool isCarrying = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (Input.touchCount > 0){
+//			networkTouch = true;
+//		} else{
+//			networkTouch = false;
+//		}
+	}
+}
--- Assets/takeMe.cs
+++ Assets/takeMe.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class takeMe : MonoBehaviour {
 
 	private Transform taker = null;
+	private Component hitCol = null;
+	private bool tempTouch = false;
+	private bool entered = false;
 	private float x,y,z;
-	public bool picked = false;
 	// Use this for initialization
 	void Start () {
 
@@ -13,17 +15,21 @@ public class takeMe : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+	
+		if(entered){
 
-//		Debug.Log(Input.touchCount);
 		// Logic to pick up enable and disable - coming from the UI
-		if (Input.touchCount > 0) {
-
+			if (hitCol.GetComponent<networkedTouchBool>().networkTouch) {
+				hitCol.GetComponent<networkedTouchBool> ().isCarrying = true;
 //		if(Input.GetKeyDown("t")) {
 //			Debug.Log ("Key down");
 //		Take only x and z co ordinate from the parent. This seems more plausible
 			transform.parent = taker;
+			
 		} else {
 			transform.parent = null;
+			hitCol.GetComponent<networkedTouchBool> ().isCarrying = false;
+//			picked = false;
 		}
 		
 		// makes the rotation locked even if the picker rotation changes
@@ -35,18 +41,33 @@ public class takeMe : MonoBehaviour {
 //
 //		}
 
+//		if (picked == true && Input.touchCount>0) {
+//			
+//			Debug.Log ("piked up!");
+//			x = taker.position.x;
+//			y = transform.position.y;
+//			z = taker.position.z;
+//			transform.position = new Vector3 (x, y, z);
+//
+//		}
+		}
+
 	}
 
 	void OnTriggerEnter (Collider col) {
 		
 
-			if(col.CompareTag("Player")) {
-				picked = true;
-				Debug.Log ("Theres contact!!!");
+		if(col.CompareTag("Player") && col.GetComponent<networkedTouchBool>().isCarrying == false) {
+				
+				entered = true;
+			
+//				Debug.Log ("Theres contact!!!");
 	//			x = col.transform.position.x;
 	//			y = transform.position.y;
 	//			z = col.transform.position.z;
 				taker = col.transform;
+				hitCol = col.GetComponent<networkedTouchBool>();
+//				col.GetComponent<networkedTouchBool> ().isCarrying = true;
 	//			taker.transform.position = new Vector3(x,y,z);
 			}
 		
@@ -56,7 +77,8 @@ public class takeMe : MonoBehaviour {
 	void OnTriggerExit (Collider col) {
 
 		if(col.CompareTag("Player")) {
-			taker = null;
+//			taker = null;
+			entered = false;
 		}
 
 	}
